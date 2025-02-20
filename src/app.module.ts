b1159e635ff9ed874ecd555144826aec25ba3df2
matheusffs <matheus.ferreira.duesoft@gmail.com>
2025-02-20 08:39:52 -0300

    [add] modulo de configuração do banco de dados [fix] ajuste do carregamento da variável de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,29 +4,16 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthGuard } from './auth/auth.guard';
 import { ClientsModule } from './clients/clients.module';
 import { IntegrationsModule } from './integrations/integrations.module';
 import { CryptoModule } from './crypto/crypto.module';
+import { DatabaseModule } from './database/databse.module';
 
 @Module({
     imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-            envFilePath: ['.env.development.local'],
-        }),
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: process.env.DATABASE_HOST,
-            port: Number(process.env.DATABASE_PORT),
-            username: process.env.DATABASE_USERNAME,
-            password: process.env.DATABASE_PASSWORD,
-            database: process.env.DATABASE_NAME,
-            entities: [`${__dirname}/database/core/**/*.entity{.js,.ts}`],
-            migrations: [`${__dirname}/migration/{.ts,*.js}`],
-            synchronize: true, // remover em produção
-        }),
+        ConfigModule.forRoot({ isGlobal: true }),
+        DatabaseModule,
         AuthModule,
         UsersModule,
         ClientsModule,
