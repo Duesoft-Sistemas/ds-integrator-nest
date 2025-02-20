a24df9db344ed4050d5b755f793777ed2c8414a1
matheusffs <matheus.ferreira.duesoft@gmail.com>
2025-02-20 15:24:55 -0300

    [fix] cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ import { join } from 'path';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: '*',
+    });
+
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
     app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
