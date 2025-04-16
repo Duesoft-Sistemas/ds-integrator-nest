415e6c1cf77f161bb0f1d0d4afa90bb0d209dfc6
matheusffs <matheus.ferreira.duesoft@gmail.com>
2025-04-16 11:42:10 -0300

    [fix] configuração do banco de dados

--- src/database/databse.module.ts
+++ src/database/databse.module.ts
@@ -1,26 +1,42 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        entities: [`${__dirname}/core/**/*.entity{.js,.ts}`],
-        migrations: [`${__dirname}/migration/{.ts,*.js}`],
-        synchronize: configService.get<boolean>('DATABASE_SYNC', true),
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }),
+      useFactory: (configService: ConfigService) => {
+        const options: any = {};
+        const type = configService.get<string>('DATABASE_TYPE');
+
+        switch (type) {
+          case 'mongodb':
+            options.type = 'mongodb';
+            options.useUnifiedTopology = true;
+            options.url = configService.get<string>('DATABASE_CONNECTION');
+            break;
+
+          default:
+            options.type = 'postgres';
+            options.host = configService.get<string>('DATABASE_HOST');
+            options.port = configService.get<number>('DATABASE_PORT');
+            options.username = configService.get<string>('DATABASE_USERNAME');
+            options.password = configService.get<string>('DATABASE_PASSWORD');
+            options.ssl = {
+              rejectUnauthorized: false,
+            };
+            break;
+        }
+
+        options.database = configService.get<string>('DATABASE_NAME');
+        options.migrations = [`${__dirname}/migration/{.ts,*.js}`];
+        options.entities = [`${__dirname}/core/**/*.entity{.js,.ts}`];
+        options.synchronize = configService.get<boolean>('DATABASE_SYNC', false);
+
+        return options as TypeOrmModuleOptions;
+      },
     }),
   ],
 })
